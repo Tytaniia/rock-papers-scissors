387ecd1d84f369fc8ee80626ecc92392c6c69906
Tytaniia <tjohnson.work0@gmail.com>
2024-01-30 11:55:31 -0600

    started js, made random function run in console. started the ps vs player section

---
+++ script.js
@@ -0,0 +1,27 @@
+function getComputerChoice(arr) {
+
+const randomIndex = Math.floor(Math.random() * arr.length);
+
+ const item = arr[randomIndex];
+
+ return item;
+}
+
+const array = ["rock", "paper", "scissors"];
+
+
+
+const result = getComputerChoice(array);
+
+console.log(result);
+
+function playerSelection() {
+
+}
+
+function computerSelection() {
+    
+}
+
+
+   
