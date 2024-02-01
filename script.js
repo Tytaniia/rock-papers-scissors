7c2aa05279e41b0e0cb490fc2a0c697b025df67f
Tytaniia <tjohnson.work0@gmail.com>
2024-01-31 18:54:05 -0600

    done

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-function getComputerChoice(arr) {
-
-const randomIndex = Math.floor(Math.random() * arr.length);
-
- const item = arr[randomIndex];
-
- return item;
+const options = ['Rock', 'Paper', 'Scissors'];
+const props = {
+    'rock': {
+        weakTo: 'Paper',
+        strongTo: 'Scissors'
+    },
+    'paper': {
+        weakTo: 'Scissors',
+        strongTo: 'Rock'
+    },
+    'scissors': {
+        weakTo: 'Rock',
+        strongTo: 'Paper'
+    }
 }
 
-const array = ["rock", "paper", "scissors"];
-
-
-
-const result = getComputerChoice(array);
-
-console.log(result);
-
-function playerSelection() {
-
+function getComputerChoice() {
+    return options[Math.floor(Math.random() * 3)];
 }
 
-function computerSelection() {
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
+
+    // DRAW
+    if(playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        console.log('Draw!');
+        return null;
+    }
+        
     
+    if(props[playerSelection.toLowerCase()].weakTo === computerSelection) {
+        console.log(`You Lose!`);
+        return false;
+    }
+    else {
+        console.log(`You win!!`);
+        return true;
+    }
 }
 
+function playGame() {
+    let playerScore = computerScore = 0;
 
-   
+        let playerSelection = prompt('What is your choice? (rock, paper, scissors): ');
+        let result = playRound(playerSelection);
+
+        
+    }
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
